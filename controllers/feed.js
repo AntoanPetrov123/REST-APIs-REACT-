ba5e77840761f6ead75b43fa78f3ba7e5aa2b007
Antoan Petrov <antoan999_kn@abv.bg>
2022-06-15 11:15:56 +0300

    Create post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,24 @@
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
-  });
+    res.status(200).json({
+        posts: [{
+            _id: '123',
+            title: 'First Post',
+            content: 'This is the first post!',
+            imageUrl: 'images/hp1.png',
+            creator: {
+                name: 'Antoan'
+            },
+            date: new Date()
+        }]
+    });
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
-  });
+    const title = req.body.title;
+    const content = req.body.content;
+    // Create post in db
+    res.status(201).json({
+        message: 'Post created successfully!',
+        post: { id: new Date().toISOString(), title: title, content: content, creator: { name: 'Antoan' }, createdAt: new Date() }
+    });
 };
