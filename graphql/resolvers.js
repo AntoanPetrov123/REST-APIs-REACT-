91b325c37aff36c5ab124993cd4126ac18c09438
Antoan Petrov <antoan999_kn@abv.bg>
2022-06-21 13:23:41 +0300

    User validations

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const validator = require('validator');
+
 const User = require('../models/user');
 
 module.exports = {
@@ -6,6 +8,21 @@ module.exports = {
         const email = args.userInput.email;
         const name = args.userInput.name;
         const password = args.userInput.password;
+
+        const errors = [];
+        if (!validator.isEmail(email)) {
+            errors.push({ message: 'Email is invalid!' });
+        }
+        if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+            errors.push({ message: 'Password must be longer!' });
+        }
+
+        if (errors.length > 0) {
+            const error = new Error('Invalid input!');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
 
         const existingUser = await User.findOne({email: email});
         if (existingUser) {
